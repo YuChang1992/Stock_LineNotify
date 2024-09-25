7e7707ea918f88c075611d66527f6a426ccc5a0b
Yu Chagn <carlworm25@gmail.com>
2024-09-25 15:23:39 +0800

    修改文字說明

--- MACD.py
+++ MACD.py
@@ -1,15 +1,14 @@
-import shioaji as sj  # Shioaji是用於永豐的API客戶端
-import requests  # 獲取網頁套件
-from datetime import date, timedelta  # 處理日期和時間
+import shioaji as sj
+import requests
+from datetime import date, timedelta
 from datetime import datetime
-import pandas as pd  # 數據處理和分析的庫
+import pandas as pd
 
 
-# API Key 和 Secret Key
 API_KEY = "你的 API KEY"
 SECRET_KEY = "你的 SECRET KEY"
 
-# 股票代碼
+
 STOCK_CODE = "股票代號"
 
 
@@ -21,33 +20,29 @@ def initialize_api():
         raise ValueError("未找到帳戶")
 
 
-# 取得個股長短期MACD方向
 def get_stock_macd():
     initialize_api()
 
-    # 設定日期範圍
     delta = timedelta(days=180)
-    start_day = date.today() - delta  # 開始日期為180天前
-    end_day = date.today()  # 結束日期今天
+    start_day = date.today() - delta
+    end_day = date.today()
     
     stock = api.Contracts.Stocks[STOCK_CODE]
     
-    kbars = api.kbars(contract=stock, start=str(start_day), end=str(end_day))  # "2024-08-05"
-    df = pd.DataFrame({**kbars})  # 轉換為DataFrame陣列
+    kbars = api.kbars(contract=stock, start=str(start_day), end=str(end_day))
+    df = pd.DataFrame({**kbars})
     df.ts = pd.to_datetime(df.ts)
     df.set_index("ts", inplace=True)
 
-    name = stock.name  # 股票名稱
-    code = stock.code  # 股票代碼
-    time = df.index[-1]  # 最後一筆成交時間
+    name = stock.name
+    code = stock.code
+    time = df.index[-1]
 
-    # 每日K棒
-    df = df.resample("D").last().dropna()  # 每日最後一筆價格，並刪除缺失值
-    # 日K線資料計算短長期的MACD指標
+    df = df.resample("D").last().dropna()
     stock_macd_df = calculate_macd(df)
     stock_long_macd_df = calculate_long_macd(df)
 
-    stock_macd_message = stock_macd_notify(stock_macd_df)  # 調用函式生成股票的歷史數據
+    stock_macd_message = stock_macd_notify(stock_macd_df)
 
     stock_long_macd_message = stock_macd_notify_long_term(stock_long_macd_df)
 
@@ -63,54 +58,40 @@ def get_stock_macd():
     return msg
 
 
-# 計算某個欄的指數移動平均線EMA
 def calculate_ema(df, column, span):
     return df[column].ewm(span=span, adjust=False).mean()
 
 
-# 計算MACD
-def calculate_macd(df, short_span=12, long_span=26, signal_span=9):  # 短期指數移動平均線(EMA)的跨度(默認為12天)，長期(默認為26天)，信號線(默認為9天)
-    df["EMA_short"] = calculate_ema(df, "Close", short_span)  # 調用calculate_ema函數，計算以Close欄為基準的短期EMA
-    df["EMA_long"] = calculate_ema(df, "Close", long_span)  # 計算長期EMA
-    # 計算DIF線(MACD快線)
-    df["MACD"] = df["EMA_short"] - df["EMA_long"]  # MACD是短期EMA和長期EMA之間的差值，可顯示短期趨勢和長期趨勢之間的差異
-    # 計算DEM線、MACD慢線(訊號線)
-    df["Signal"] = calculate_ema(df, "MACD", signal_span)  # 信號線是MACD的9日平均值，是MACD的平滑版
-    # 計算MACD直方圖
-    df["Hist"] = df["MACD"] - df["Signal"]  # Histograms是MACD和Signal之間的差值，用來識別趨勢的強度和反轉信號
+def calculate_macd(df, short_span=12, long_span=26, signal_span=9):
+    df["EMA_short"] = calculate_ema(df, "Close", short_span)
+    df["EMA_long"] = calculate_ema(df, "Close", long_span)
+    df["MACD"] = df["EMA_short"] - df["EMA_long"]
+    df["Signal"] = calculate_ema(df, "MACD", signal_span)
+    df["Hist"] = df["MACD"] - df["Signal"]
     return df
 
 
-# 計算長期MACD
 def calculate_long_macd(df, short_span=60, long_span=120, signal_span=9):
     df["MACD_60"] = calculate_ema(df, "Close", short_span)
     df["MACD_120"] = calculate_ema(df, "Close", long_span)
-    # 計算DIF線(MACD快線)
     df["LONG_MACD"] = df["MACD_60"] - df["MACD_120"]
-    # 計算DEM線、MACD慢線(訊號線)
     df["LONG_Signal"] = calculate_ema(df, "LONG_MACD", signal_span)
-    # 計算MACD直方圖
     df["LONG_Hist"] = df["LONG_MACD"] - df["LONG_Signal"]
     return df
 
 
-# 判斷個股MACD的趨勢
 def stock_macd_notify(df):
 
-    # 取近三天的MACD
-    today_macd = df['MACD'].iloc[-1]  # 倒數第一筆數據
-    yesterday_macd = df['MACD'].iloc[-2]  # 倒數第二筆數據
-    two_days_ago_macd = df['MACD'].iloc[-3]  # 倒數第三筆數據
-    # 取近三天的Signal信號線
+    today_macd = df['MACD'].iloc[-1]
+    yesterday_macd = df['MACD'].iloc[-2]
+    two_days_ago_macd = df['MACD'].iloc[-3]
     today_signal = df['Signal'].iloc[-1]
     yesterday_signal = df['Signal'].iloc[-2]
     two_days_ago_signal = df['Signal'].iloc[-3]
-    # 取近兩天的Histogram直方圖
     today_hist = df['Hist'].iloc[-1]
     yesterday_hist = df['Hist'].iloc[-2]
 
     hist_message = ""
-    # 判斷Histogram直方圖紅綠轉換
     if today_hist > 0:
         if yesterday_hist > 0:
             if today_hist > yesterday_hist:
@@ -131,30 +112,24 @@ def stock_macd_notify(df):
         hist_message = "無明顯變化，無法判斷趨勢"
 
     message = ""
-    # 判斷 MACD 線和 Signal 線的交叉
     if two_days_ago_macd <= two_days_ago_signal and yesterday_macd > yesterday_signal and today_macd > today_signal:
-        # MACD 線從下方連續上穿 Signal 線
         message = "黃金交叉，顯示買入信號"
     elif two_days_ago_macd >= two_days_ago_signal and yesterday_macd < yesterday_signal and today_macd < today_signal:
-        # MACD 線從上方連續下穿 Signal 線
         message = "死亡交叉，顯示賣出信號"
     else:
-        # 無交叉信號情況
         message = "無明顯交叉信號，繼續觀察"
     
     response_message = f"{hist_message}\n{message}"
 
-    return response_message  # 回傳趨勢的通知訊息
+    return response_message
 
 
-# 判斷個股長期MACD
 def stock_macd_notify_long_term(df):
     last_hist = df['Hist'].iloc[-1]
     hist_mean = df["Hist"].mean()
 
     hist_message = ""
 
-    # 判斷Histogram直方圖紅綠轉換
     if last_hist > 0:
         if hist_mean > 0:
             hist_message = "上升趨勢增長，顯示長期潛力"
@@ -171,7 +146,6 @@ def stock_macd_notify_long_term(df):
     crossover_count = sum((df['MACD'].iloc[-i] > df['Signal'].iloc[-i]) for i in range(1, 21))
     previous_count = sum((df['MACD'].iloc[-i] < df['Signal'].iloc[-i]) for i in range(1, 21))
 
-    # 判斷MACD線和Signal線的交叉
     if crossover_count >= 10:
         message = "潛在買入機會，考慮進場"
     elif previous_count >= 10:
@@ -179,7 +153,7 @@ def stock_macd_notify_long_term(df):
     
     response_message = f"{hist_message}\n{message}"
 
-    return response_message  # 回傳趨勢的通知訊息
+    return response_message
 
 
 def send_line_notify(message):
@@ -190,7 +164,7 @@ def send_line_notify(message):
 
     param = {'message': f'{message}'}
 
-    data = requests.post(url, headers=headers, params=param)  # requests.post向伺服器提交資源或數據  data使用字典的方式傳送資料
+    data = requests.post(url, headers=headers, params=param)
     print(data)
 
 
--- SinoPac_API.py
+++ SinoPac_API.py
@@ -1,7 +1,7 @@
-import shioaji as sj  # Shioaji是用於永豐的API客戶端
-import json  # 處理JSON格式的數據
-import pandas as pd  # 數據處理和分析的庫
-from datetime import date, timedelta  # 處理日期和時間
+import shioaji as sj
+import json
+import pandas as pd
+from datetime import date, timedelta
 
 api = None
 accounts = None
@@ -37,46 +37,46 @@ def lambda_handler(event, context):
                     "person_id": accounts[0].person_id,
                     "broker_id": accounts[0].broker_id,
                     "account_id": accounts[0].account_id,
-                    "username": accounts[0].username}  # 返回第一個帳戶的帳戶資料
+                    "username": accounts[0].username}
 
         elif action == "get_ma_stop_loss":
-            stock_code = event.get("stock_code")  # 提取股票代碼
-            buy_price = event.get("buy_price")  # 提取買入價格
-            ma_diff_data = get_current_ma_diff(event, stock_code, buy_price)  # 調用get_current_ma_diff函數獲取MA跟買價的價差
-            return {
-                "statusCode": 200,  # 返回請求成功
-                "body": json.dumps(
-                    {"account_id": accounts[0].account_id,
-                    "responseData": ma_diff_data}  # 返回帳戶ID和MA跟買價的價差
-                )
-            }
-        elif action == "get_index_info":
-            index_info = get_index_information(event)  # 調用get_index_information函數獲取加權指數&櫃買指數的MACD方向
+            stock_code = event.get("stock_code")
+            buy_price = event.get("buy_price")
+            ma_diff_data = get_current_ma_diff(event, stock_code, buy_price)
             return {
                 "statusCode": 200,
                 "body": json.dumps(
                     {"account_id": accounts[0].account_id,
-                    "responseData": index_info}  # 返回帳戶ID和加權指數&櫃買指數的MACD方向
+                    "responseData": ma_diff_data}
+                )
+            }
+        elif action == "get_index_info":
+            index_info = get_index_information(event)
+            return {
+                "statusCode": 200,
+                "body": json.dumps(
+                    {"account_id": accounts[0].account_id,
+                    "responseData": index_info}
                 )
             }
         elif action == "get_stock_macd":
             stock_code = event.get("stock_code")
-            stock_macd = get_stock_macd(event, stock_code)  # 調用get_stock_macd函數獲取MACD指標數據
+            stock_macd = get_stock_macd(event, stock_code)
             return {
                 "statusCode": 200,
                 "body": json.dumps(
                     {"account_id": accounts[0].account_id,
-                    "responseData": stock_macd}  # 返回帳戶ID和MACD指標數據
+                    "responseData": stock_macd}
                 )
             }
         elif action == "get_now_stock":
             stock_code = event.get("stock_code")
-            up_down = get_now_stock(event, stock_code)  # 調用get_now_stock函數獲取個股資訊
+            up_down = get_now_stock(event, stock_code)
             return {
                 "statusCode": 200,
                 "body": json.dumps(
                     {"account_id": accounts[0].account_id,
-                    "responseData": up_down}  # 返回帳戶ID和和個股漲跌
+                    "responseData": up_down}
                 )
             }
         else:
@@ -84,7 +84,7 @@ def lambda_handler(event, context):
                 "statusCode": 400,
                 "body": json.dumps(
                     {"account_id": accounts[0].account_id,
-                    "responseData": "發生錯誤"}  # 返回帳戶ID和錯誤信息
+                    "responseData": "發生錯誤"}
                 )
             }
     except Exception as e:
@@ -94,12 +94,10 @@ def lambda_handler(event, context):
 def calculate_moving_averages(event, stock_code, window=60, buy_price=None):
     get_api_and_accounts(event)
 
-    # 設定日期範圍
     today = date.today()
     delta = timedelta(days=180)
     date_180 = today - delta
     
-    # 取得指定股票代碼在指定日期範圍內的K線數據
     kbars = api.kbars(
         contract=api.Contracts.Stocks[stock_code],
         start=str(date_180),
@@ -112,14 +110,12 @@ def calculate_moving_averages(event, stock_code, window=60, buy_price=None):
 
     df = df.resample("D").last().dropna()
 
-    # 計算移動平均線
-    df["5MA"] = df["Close"].rolling(window=5).mean().round(2)  # 5日
-    df["10MA"] = df["Close"].rolling(window=10).mean().round(2)  # 10日
-    df["20MA"] = df["Close"].rolling(window=20).mean().round(2)  # 20日(月線)
-    df["60MA"] = df["Close"].rolling(window=60).mean().round(2)  # 60日(季線)
-    df["20MA/60MA"] = (df["20MA"] / df["60MA"]).round(2)  # 計算20日移動平均線與60日移動平均線的比率
+    df["5MA"] = df["Close"].rolling(window=5).mean().round(2)
+    df["10MA"] = df["Close"].rolling(window=10).mean().round(2)
+    df["20MA"] = df["Close"].rolling(window=20).mean().round(2)
+    df["60MA"] = df["Close"].rolling(window=60).mean().round(2)
+    df["20MA/60MA"] = (df["20MA"] / df["60MA"]).round(2)
 
-    # 計算與買價的百分比差距
     if buy_price is not None:
         df["5MA_diff"] = ((buy_price - df["5MA"]) / buy_price * 100).round(2)
         df["10MA_diff"] = ((buy_price - df["10MA"]) / buy_price * 100).round(2)
@@ -141,7 +137,6 @@ def calculate_moving_averages(event, stock_code, window=60, buy_price=None):
     ].dropna()
 
 
-# 計算與買價的價差
 def get_current_ma_diff(event, stock_code, buy_price=None):
     ma_data = calculate_moving_averages(event, stock_code, buy_price=buy_price)
     current_5ma_diff = ma_data["5MA_diff"].iloc[-1]
@@ -166,21 +161,17 @@ def get_current_ma_diff(event, stock_code, buy_price=None):
     # print("已成功寫入 ma_diff.json")
 
 
-# 取加權指數&櫃買指數的MACD方向
 def get_index_information(event):
     get_api_and_accounts(event)
     
-    # 設定日期範圍
     delta = timedelta(days=180)
     start_day = date.today() - delta
     end_day = date.today()
 
-    tse_index = api.Contracts.Indexs.TSE["001"]  # 獲取加權指數（TSE）
-    otc_index = api.Contracts.Indexs.OTC["101"]  # 獲取櫃買指數（OTC）
+    tse_index = api.Contracts.Indexs.TSE["001"]
+    otc_index = api.Contracts.Indexs.OTC["101"]
 
-    # 取得加權指數資料在指定日期範圍內的K線數據
     tse_kbars = api.kbars(contract=tse_index, start=str(start_day), end=str(end_day))
-    # 取得櫃買指數資料在指定日期範圍內的K線數據
     otc_kbars = api.kbars(contract=otc_index, start=str(start_day), end=str(end_day))
 
     tse_df = pd.DataFrame({**tse_kbars})
@@ -193,15 +184,12 @@ def get_index_information(event):
     tse_daily_df = tse_df.resample("D").last().dropna()
     otc_daily_df = otc_df.resample("D").last().dropna()
 
-    # 對加權指數和櫃買指數的日K數據計算MACD指標
     tse_macd_df = calculate_macd(tse_daily_df)
     otc_macd_df = calculate_macd(otc_daily_df)
 
-    # 生成加權指數和櫃買指數的MACD通知訊息
     tse_response_msg = index_macd_notify(tse_macd_df["Hist"])
     otc_response_msg = index_macd_notify(otc_macd_df["Hist"])
 
-    # 獲取今日前100名漲跌家數
     amountRankChangeCount = getAmountRankChangeCount(event)
 
     result = {
@@ -221,7 +209,6 @@ def truncate_to_two_decimal_places(value):
     return float(f"{value:.2f}")
 
 
-# 取得個股即時資訊
 def get_now_stock(event, stock_code):
     get_api_and_accounts(event)
 
@@ -255,11 +242,9 @@ def get_now_stock(event, stock_code):
         print(f"下跌{percentage_float}% 建議少量買入，觀察是否反彈")
     
 
-# 取個股MACD方向
 def get_stock_macd(event, stock_code):
     get_api_and_accounts(event)
 
-    # 設定日期範圍
     delta = timedelta(days=180)
     start_day = date.today() - delta
     end_day = date.today()
@@ -287,82 +272,73 @@ def get_stock_macd(event, stock_code):
     # print("已成功寫入 stock_macd.json")
 
 
-# 計算指數移動平均線EMA
 def calculate_ema(df, column, span):
     return df[column].ewm(span=span, adjust=False).mean()
 
 
-# 計算MACD
-def calculate_macd(df, short_span=12, long_span=26, signal_span=9):  # 短期EMA的跨度(默認為12天)，長期(默認為26天)，信號線(默認為9天)
-    df["EMA_short"] = calculate_ema(df, "Close", short_span)  # 計算短期EMA
-    df["EMA_long"] = calculate_ema(df, "Close", long_span)  # 計算長期EMA
-    # 計算MACD值
-    df["MACD"] = df["EMA_short"] - df["EMA_long"]  # MACD是短期EMA減長期EMA
-    # 計算EMA信號線
-    df["Signal"] = calculate_ema(df, "MACD", signal_span)  # 信號線是MACD的9日平均值
-    # 計算MACD直方圖
-    df["Hist"] = df["MACD"] - df["Signal"]  #顯示MACD的波動幅度，是MACD和信號線之間的差值
+def calculate_macd(df, short_span=12, long_span=26, signal_span=9):
+    df["EMA_short"] = calculate_ema(df, "Close", short_span)
+    df["EMA_long"] = calculate_ema(df, "Close", long_span)
+    df["MACD"] = df["EMA_short"] - df["EMA_long"]
+    df["Signal"] = calculate_ema(df, "MACD", signal_span)
+    df["Hist"] = df["MACD"] - df["Signal"]
     return df
 
 
-# 判斷加權指數跟櫃買指數MACD的趨勢
 def index_macd_notify(df):
-    today_hist = df.iloc[-1]  # 取列表中倒數第一筆數據，是今天的MACD直方圖
-    yesterday_hist = df.iloc[-2]  # 取列表中倒數第二筆數據，是昨天的MACD直方圖
+    today_hist = df.iloc[-1]
+    yesterday_hist = df.iloc[-2]
 
-    if today_hist > 0:  # 今日是紅柱
-        if yesterday_hist < 0:  # 昨天是綠柱，今天轉紅柱
+    if today_hist > 0:
+        if yesterday_hist < 0:
             response_message = "『綠轉紅』市場情緒由負轉正，觀察是否有進一步增長的空間。\n"
             response_message += "嘗試尋找強勢族群進場，仍需保持謹慎。"
-        elif today_hist > yesterday_hist:  # 今日紅柱比昨日增長
+        elif today_hist > yesterday_hist:
             response_message = "『紅柱增長』可以積極做多\n"
             response_message += "找到強勢族群中最好的，做好做滿。"
-        else:  # 今日紅柱比昨日縮短
+        else:
             response_message = "『紅柱縮短』降低槓桿跟部位，不再買入，留倉觀察"
-    elif today_hist < 0:  # 今日是綠柱
-        if yesterday_hist > 0:  # 昨天是紅柱，今天轉綠柱
+    elif today_hist < 0:
+        if yesterday_hist > 0:
             response_message = "『紅轉綠』市場情緒由正轉負，小心進一步下跌風險。\n"
             response_message += "建議減少倉位，觀望更明顯的反轉訊號。"
-        elif today_hist < yesterday_hist:  # 今日綠柱比昨日增長
+        elif today_hist < yesterday_hist:
             response_message = "『綠柱增長』不要看盤了，買了錢會賠光光\n"
             response_message += "禁止買入任何部位，嚴禁抄底\n"
             response_message += "如果持股已經套牢，彈上來是給你停損的"
-        else:  # 今日綠柱比昨日縮短
+        else:
             response_message = "『綠柱縮短』可以嘗試做多強勢族群，不上槓桿，嚴守停損\n"
             response_message += "記住這是搶反彈，停損一定要守在成本\n"
             response_message += "如果持股已經套牢，彈上來是給你停損的"
     else:
         response_message = "直方圖無變化，保持觀望。"
 
-    return response_message  # 回傳直方圖趨勢的通知訊息
+    return response_message
 
 
-# 判斷個股MACD的趨勢
 def stock_macd_notify(df):
-    today_hist = df.iloc[-1]  # 取列表中倒數第一筆數據，是今天的MACD直方圖值
-    yesterday_hist = df.iloc[-2]  # 取列表中倒數第二筆數據，是昨天的MACD直方圖值
+    today_hist = df.iloc[-1]
+    yesterday_hist = df.iloc[-2]
 
-    if today_hist > 0:  # 如果今天的直方圖大於0(紅柱)
-        if today_hist > yesterday_hist:  # 如果今天的紅柱比昨天增長
+    if today_hist > 0:
+        if today_hist > yesterday_hist:
             response_message = "『紅柱增長』續抱"
-        else:  # 如果今天的紅柱比昨天縮短
+        else:
             response_message = "『紅柱縮短』停利降低部位"
-    else:  # 如果今天的直方圖小於0(綠柱)
-        if today_hist < yesterday_hist:  # 如果今天的綠柱比昨天增長
+    else:
+        if today_hist < yesterday_hist:
             response_message = "『綠柱增長』不要買進"
-        else:  # 如果今天的綠柱比昨天縮短
+        else:
             response_message = "『綠柱縮短』嘗試抄底建倉，小量進場試單"
 
-    return response_message  # 回傳直方圖趨勢的通知訊息
+    return response_message
 
 
-# 排行前100名的漲跌家數
 def getAmountRankChangeCount(event):
     get_api_and_accounts(event)
 
     today = date.today()
 
-    # 獲取今天的數據，按成交金額排序，取前100的股票
     scanners = api.scanners(
         scanner_type=sj.constant.ScannerType.AmountRank, count=100, date=str(today)
     )
@@ -370,19 +346,17 @@ def getAmountRankChangeCount(event):
     df = pd.DataFrame(s.__dict__ for s in scanners)
     df.ts = pd.to_datetime(df.ts)
 
-    # 取change_type,計算漲停 上漲 下跌 跌停 平盤家數
     changeTypeDf = df.change_type.value_counts()
 
     for i in range(1, 6):
         if changeTypeDf.get(i, 0) == 0:
             changeTypeDf[i] = 0
 
-    # 獲取每種類型的數量
-    limitUpAmount = int(changeTypeDf[1])  # 漲停
-    upAmount = int(changeTypeDf[2])  # 上漲
-    unChangeAmount = int(changeTypeDf[3])  # 平盤
-    downAmount = int(changeTypeDf[4])  # 下跌
-    limitDownAmount = int(changeTypeDf[5])  # 跌停
+    limitUpAmount = int(changeTypeDf[1])
+    upAmount = int(changeTypeDf[2])
+    unChangeAmount = int(changeTypeDf[3])
+    downAmount = int(changeTypeDf[4])
+    limitDownAmount = int(changeTypeDf[5])
 
     result = {
         "漲停": limitUpAmount,
@@ -393,7 +367,6 @@ def getAmountRankChangeCount(event):
     }
     return result
 
-#--------------------------------------------------
 
 if __name__ == "__main__":
     event = {
@@ -403,10 +376,8 @@ if __name__ == "__main__":
         "stock_code": "股票代碼",
         "buy_price": "買入價格(數值)"
     }
-    context = {}  # 在本地測試時，這個可以是空的
+    context = {}
 
-    # 調用 lambda_handler 函式
     response = lambda_handler(event, None)
 
-    # 打印返回結果
     print(response)
